2d83630d382b5e7d3a5faca32784f6f08d5d85d0
jan p springer <jsd@igroup.org>
2016-04-14 21:33:29 +0100

    added: initial structure

---
+++ cmake/HUGH_DATA_SetupVariables.cmake
@@ -0,0 +1,14 @@
+# -*- Mode:cmake -*-
+
+####################################################################################################
+#                                                                                                  #
+# Copyright (C) 2016 University of Hull                                                            #
+#                                                                                                  #
+####################################################################################################
+
+if(VERBOSE)
+  message(STATUS "Loading ${PROJECT_NAME} variables")
+endif()
+
+include(CMakeAddons_SetupVariables)
+
